c4e3c6c0df2342b45ec464f348fdaa60bcd1abdb
Alex sheffcer <a.sheffcer@gmail.com>
2021-01-25 13:18:29 +0200

    added image attribute

--- src/components/AppAvatar.vue
+++ src/components/AppAvatar.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="avatar">
-    <img src="https://cdn.dribbble.com/users/5592443/screenshots/14279501/drbl_pop_r_m_rick_4x.png">
+    <img :src="message" v-bind="$attrs">
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    message: String
+  }
 }
 </script>
 
